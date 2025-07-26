b8b261d3b62852f0a8c3e863f6fd6b1545de78dc
Vikrant Mulankar <vikranmulankar389@gmail.com>
2025-07-26 13:36:37 +0530

    Your commit message here

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,8 +12,7 @@ const Card = () => {
         <div className="cursor-pointer group w-full sm:max-w-full md:max-w-full lg:max-w-[400px] xl:max-w-[420px] bg-gradient-to-br from-[#0f172a] to-[#1e272e] text-white rounded-2xl shadow-md !p-5 flex flex-row lg:flex-col-reverse xl:flex-row items-center justify-between transition-all duration-300 hover:scale-[1.03] hover:shadow-lg ">
           <div className="!space-y-2 sm:!space-y-2.5 lg:text-center xl:text-left">
             <h4 className="!text-xl sm:!text-xl md:!text-xl lg:!text-xl xl:!text-lg font-semibold tracking-wide transition-all duration-300 group-hover:bg-gradient-to-r group-hover:from-[#A9C5E1] group-hover:to-[#ff9966] group-hover:bg-clip-text group-hover:text-transparent !m-0">
-              {/* Total Patients */}
-              Vikrant Mulankar
+              Total Patients
             </h4>
             <h2 className="!text-xl sm:!text-2xl md:!text-2xl lg:!text-xl xl:!text-2xl font-bold transition-all duration-300 group-hover:bg-gradient-to-r group-hover:from-[#A9C5E1] group-hover:to-[#ff9966] group-hover:bg-clip-text group-hover:text-transparent">
               165
